1b671eeb87465721ad142afb92a4ca6d443c1b4e
Awais Amjad <2022cs541@student.uet.edu.pk>
2024-11-11 20:17:30 +0500

    master

--- test_project/urls.py
+++ test_project/urls.py
@@ -2,8 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from accounts.views import home,hello_world
+from accounts.views import home, signup_view, login_view, logout_view
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
+    path('', home, name='home'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('admin/', admin.site.urls),
 ]
